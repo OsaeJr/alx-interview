73cbadd5e2c6ed1b4203fdb962fc823b0d382cc0
OsaeJr <samueladjar11@gmail.com>
2024-10-16 08:48:24 -0700

    Add files

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,45 +1,35 @@
 #!/usr/bin/python3
-"""
-Calculates the minimum number of operations needed to achieve 'n' characters
-by using dynamic programming.
-"""
+'''The minimum operations coding challenge.
+'''
 
 
 def minOperations(n):
-    """
-    Calculates the minimum number of operations needed to achieve 'n'
-    characters.
-
-    Args:
-    - n (int): The target number of characters.
-
-    Returns:
-    - int: The minimum number of operations required.
-    """
-
-    if n <= 1:
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
-
-    # Create a list to store the minimum operations needed for each count of
-    # characters
-    operations_needed = [0] * (n + 1)
-
-    # Iterate through the range of 2 to n to calculate minimum operations for
-    # each count
-    for i in range(2, n + 1):
-        # Initialize the number of operations for i characters to i
-        # (maximum number of operations)
-        operations_needed[i] = i
-
-        # Iterate through possible factors j of i to find the minimum
-        # operations
-        for j in range(2, i // 2 + 1):
-            # Check if j is a factor of i
-            if i % j == 0:
-                # Update operations_needed[i] with the minimum operations found
-                # so far
-                operations_needed[i] = min(
-                        operations_needed[i], operations_needed[j] + i // j)
-
-    # Return the minimum operations needed to achieve n characters
-    return operations_needed[n]
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
